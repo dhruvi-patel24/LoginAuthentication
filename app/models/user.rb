dc00f4cddbb3de039f813b4fef2c8796ad78d798
patel24dhruvi <patel24dhruvi@gmail.com>
2022-06-29 07:32:32 +0530

    app/models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  belongs_to :role
+  def admin?
+    role.name == 'admin'
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
