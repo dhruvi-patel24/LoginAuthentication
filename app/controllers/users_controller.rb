0e43e437485b3e5e7956d2992198b28f980d4da9
patel24dhruvi <patel24dhruvi@gmail.com>
2022-06-29 11:45:45 +0530

    controllerfiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @q = User.ransack(params[:q])
-    @users = @q.result
+    @users = @q.result.page(params[:page]).per(2)
   end
 end
